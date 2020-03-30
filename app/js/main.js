3432cdadce0bdfafc755893ac88bb73191eebe40
Valeriy Mikheyev <valeriy.mikheyev@gmail.com>
2020-03-30 17:52:49 +0300

    Fixed modal mobile open

--- app/js/main.js
+++ app/js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     $("#nav-mobile").toggleClass("active");
   });
 
-  $("#modal-open").on("click", function (event) {
+  $(".modal-open").on("click", function (event) {
     event.preventDefault();
     $("#nav-mobile").toggleClass("active");
   });
