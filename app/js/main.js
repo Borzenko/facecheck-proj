634b1fd406be934410861dc54fe5cbd252f51ccd
Valeriy Mikheyev <valeriy.mikheyev@gmail.com>
2020-03-30 17:35:21 +0300

    Foxed header and mobile lines

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     easing: 'easeInOutSine'
   });
   $('.mobile-inner .mobile-wrapper:last')
-  .css('border-bottom', 'none');
+    .css('border-bottom', 'none');
   $("#nav_toggle").on("click", function (event) {
     event.preventDefault();
     $(this).toggleClass("active");
@@ -31,8 +31,8 @@ $(document).ready(function () {
     event.preventDefault();
     $("#nav-mobile").toggleClass("active");
     $([document.documentElement, document.body]).animate({
-      scrollTop: $(".main-features").offset().top -15
-  }, 500);
+      scrollTop: $(".main-features").offset().top - 15
+    }, 500);
   });
 
   $("#mobile-home").on("click", function (event) {
@@ -40,15 +40,15 @@ $(document).ready(function () {
     $("#nav-mobile").toggleClass("active");
     $([document.documentElement, document.body]).animate({
       scrollTop: $(".download-desktop-app").offset().top
-  }, 500);
+    }, 500);
   });
 
-  $('.description').on('input', function(e) {
+  $('.description').on('input', function (e) {
     var textEntered, countRemaining, counter;
     textEntered = $(this).val();
     counter = (450 - (textEntered.length));
     countRemaining = document.getElementById('charactersRemaining');
-    countRemaining.innerText= counter;
+    countRemaining.innerText = counter;
   })
 
   var carousel = $("#owl-demo").owlCarousel({
@@ -60,52 +60,52 @@ $(document).ready(function () {
     autoplaySpeed: 1000,
     autoplayTimeout: 5000,
     loop: true,
-    touchDrag:false,
+    touchDrag: false,
     mouseDrag: false
   });
 
   $('.profile-card[data-slide="0"').addClass('active')
-  
-  $('.profile-card').click(function() {
+
+  $('.profile-card').click(function () {
     $('.profile-card').removeClass('active')
     $(this).addClass('active')
 
     carousel.trigger('to.owl.carousel', [$(this).data().slide, 300]);
   })
 
-  carousel.on('changed.owl.carousel', function(event) {
+  carousel.on('changed.owl.carousel', function (event) {
     $('.profile-card').removeClass('active')
     $($('.profile-cards-desktop .profile-card')[event.page.index]).addClass('active');
     $($('.profile-cards-mobile .profile-card')[event.page.index]).addClass('active');
   })
 
-  $('.drop-down-arrow',).click(function() {
+  $('.drop-down-arrow', ).click(function () {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $('.main-features').offset().top
+      scrollTop: $('.main-features').offset().top
     }, 500);
   });
 
-  $('#features-scroll',).click(function() {
+  $('#features-scroll', ).click(function () {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $('.main-features').offset().top
+      scrollTop: $('.main-features').offset().top
     }, 500);
   });
 
-  $('#home-scroll',).click(function() {
+  $('#home-scroll', ).click(function () {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $('.download-desktop-app').offset().top
+      scrollTop: $('.download-desktop-app').offset().top
     }, 500);
   });
-  
+
   AOS.init({
     duration: 3200
   });
 
-  $('.field').on('keyup', function(){
+  $('.field').on('keyup', function () {
     $(this).removeClass('error');
   })
 
-  $('.modal-open').on('click', function() {
+  $('.modal-open').on('click', function () {
     $('#modal-contact').modal({
       escapeClose: false,
       clickClose: false,
@@ -114,7 +114,7 @@ $(document).ready(function () {
   })
 
 
-  $('.modal-form').submit(function(e) {
+  $('.modal-form').submit(function (e) {
     e.preventDefault();
     var name = $('#name').val();
     var email = $('#email').val();
@@ -123,43 +123,41 @@ $(document).ready(function () {
     var fields = $('.field');
     $("input").removeClass('error');
     $("textarea").removeClass('error')
- 
+
     required(name, '#name')
     required(subject, '#subject')
     required(message, '.description')
     required(email, '#email')
 
-    if(isValidEmail(email) == false) { 
+    if (isValidEmail(email) == false) {
       $('#email').addClass('error');
     }
 
     $('span.accepted').remove();
     $('span.error-text').remove();
-    
+
     if ($(fields).hasClass('error')) {
-        $('.submit-modal').before('<span class="error-text">Make sure all fields are correct</span>');
+      $('.submit-modal').before('<span class="error-text">Make sure all fields are correct</span>');
     } else {
-        $('.submit-modal').before('<span class="accepted">Message sent - thank you!</span>');
-        setTimeout(function() {
-          $('.close_modal').trigger('click')
-        }, 1500)
+      $('.submit-modal').before('<span class="accepted">Message sent - thank you!</span>');
+      setTimeout(function () {
+        $('.close_modal').trigger('click')
+      }, 1500)
     }
   });
-});  
+});
 
-var required = function(value, selector) {
+var required = function (value, selector) {
   if (!value.length) {
     $(selector).addClass('error')
   }
 }
 
-var isValidEmail = function(email) {
+var isValidEmail = function (email) {
   var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if(!regex.test(email)) {
+  if (!regex.test(email)) {
     return false;
-  }else{
+  } else {
     return true;
   }
 }
-
-
